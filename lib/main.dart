50e41eccf892efd79649492f175271c52d062611
caiosibrao <caiohenriquesibrao22@gmail.com>
2025-05-06 17:31:52 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:app_fluxolivrep/src/pages/admin_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const AdminPage());
+}
---
+++ lib/src/pages/admin_page.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const AdminPage());
+}
+
+class AdminPage extends StatelessWidget {
+  const AdminPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const AdminPanel(), // <- AQUI: renderiza AdminPanel como conteúdo principal
+    );
+  }
+}
+
+class AdminPanel extends StatelessWidget {
+  const AdminPanel({super.key});
+
+  final List<Map<String, dynamic>> items = const [
+    {'title': 'Usuários', 'image': Icons.group},
+    {'title': 'Relatório', 'image': Icons.assignment},
+    {'title': 'Configurações', 'image': Icons.settings},
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: const Text('Fluxo Livre - ADMIN'),
+        actions: [
+          IconButton(onPressed: () {}, icon: const Icon(Icons.settings)),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10,
+          ),
+          itemCount: items.length,
+          itemBuilder: (context, index) => InkWell(
+            onTap: () {},
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.grey[200],
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: const [
+                  BoxShadow(
+                    color: Colors.black12,
+                    blurRadius: 4,
+                    offset: Offset(2, 2),
+                  ),
+                ],
+              ),
+              child: Stack(
+                alignment: Alignment.bottomLeft,
+                children: [
+                  Center(
+                    child: Icon(
+                      items[index]['image'],
+                      size: 80,
+                      color: Colors.blueAccent,
+                    ),
+                  ),
+                  Container(
+                    width: double.infinity,
+                    padding: const EdgeInsets.all(8),
+                    decoration: const BoxDecoration(
+                      color: Colors.black45,
+                      borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(10),
+                        bottomRight: Radius.circular(10),
+                      ),
+                    ),
+                    child: Text(
+                      items[index]['title'],
+                      style: const TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
